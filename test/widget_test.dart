33b632983676e634465e4005f57302994d814735
vikram <vikram.sharma.main@gmail.com>
2025-08-21 15:35:06 +0800

    Updated chat interface and fixed gradient background

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:chatbot_mobile_app/main.dart';
+import 'package:quantisage_travel_assistant/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
